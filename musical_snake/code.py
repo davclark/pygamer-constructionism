c6fd23ca326647c77370848c7da65ab8ce564e9a
Dav Clark <dav.clark@noteable.io>
2022-05-20 16:05:58 -0700

    wraparound

--- musical_snake/code.py
+++ musical_snake/code.py
@@ -53,14 +53,14 @@ display.show(meta_group)
 
 # Loop through each sprite in the sprite sheet
 source_index = 0
-thresholds = {
+thresh = {
     'right': 36000,
     'left': 30000,
     'up': 35000,
     'down': 29000,
 }
-delta_x = 1
-delta_y = 1
+delta_x = 0
+delta_y = 0
 while True:
     # When you change the sprite, it seems to mess up the location?
     # sprite[0] = source_index % 6
@@ -69,33 +69,42 @@ while True:
 
     # We include ONLY cases where the joystick is pressed in some direction
     # If we're in the middle, we don't update
-    if x > 36000:
+    if x > thresh['right']:
         delta_x = 1
-        if y > 33000:
+        if y > thresh['up']:
             delta_y = 1
-        elif y < 31000:
+        elif y < thresh['down']:
             delta_y = -1
         else:
             delta_y = 0
-    elif x < 30000:
+    elif x < thresh['left']:
         delta_x = -1
-        if y > 33000:
+        if y > thresh['up']:
             delta_y = 1
-        elif y < 31000:
+        elif y < thresh['down']:
             delta_y = -1
         else:
             delta_y = 0
     else:
-        if y > 33000:
+        if y > thresh['up']:
             delta_y = 1
             delta_x = 0
-        elif y < 31000:
+        elif y < thresh['down']:
             delta_y = -1
             delta_x = 0
 
     sprite_group.x += delta_x
     sprite_group.y += delta_y
 
+    if sprite_group.x < -16:
+        sprite_group.x = 160 - 16
+    if sprite_group.x > 160 - 16:
+        sprite_group.x = -16
+
+    if sprite_group.y < -16:
+        sprite_group.y = 128 - 16
+    if sprite_group.y > 128 - 16:
+        sprite_group.y = -16
     time.sleep(0.1)
 
 # Square with dots
